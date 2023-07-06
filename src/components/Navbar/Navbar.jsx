482510f3af6a5c0b45ae712286884516247e6196
Diego Galdino <diegogaldino-dev@gmail.com>
2023-07-06 15:12:19 -0300

    adjusting navbar details

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,59 +1,67 @@
-import React from 'react';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
-import { FaFacebook, FaInstagram } from 'react-icons/fa';
-import {LogoImage, NavRedes, CustomNavbar, TextLogo, StyledNavDropdown} from './style'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
+import {
+  TextLogo,
+  StyledNavDropdown,
+  StyledNavbar,
+  TextContainer,
+} from "./style";
+import { Link } from "react-router-dom";
 
-import Logo from '../../img/logo_oficial 1.svg'
-
+import Logo from "../../img/logo_oficial 1.svg";
 
 const NavBar = () => {
   return (
-    <CustomNavbar bg="" expand="lg" className="shadow">
-    <Navbar.Brand href="/">
-      <LogoImage
-        src={Logo} // substitua pelo caminho da imagem do seu logotipo
-        height="60"
-        className="d-inline-block align-top "
-        alt="Logo"
-      />
-      <TextLogo>DDS - Agencia de Desenvolvimento</TextLogo>
-    </Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-      <Nav className="ml-auto">
-        <Nav.Link as={Link} to="/" style={{color:"#fff"}}>
-          Inicio
-        </Nav.Link>
-        <Nav.Link as={Link} to="/quem-somos" style={{color:"#fff"}}>
-          Quem Somos
-        </Nav.Link>
-        <Nav.Link as={Link} to="/contato" style={{color:"#fff"}}>
-          Contato
-        </Nav.Link>
-        <StyledNavDropdown title="Tipos de Serviços" id="basic-nav-dropdown">
-          <NavDropdown.Item as={Link} alt="Criação de Sites" to="/criacao-de-sites">
-            Criação de Sites
-          </NavDropdown.Item>
-          <NavDropdown.Item as={Link} to="/marketing-digital" >
-            Marketing Digital
-          </NavDropdown.Item>
-          <NavDropdown.Item as={Link} to="/social-midia">
-            Social Midia
-          </NavDropdown.Item>
-        </StyledNavDropdown>
-      </Nav>
-      <NavRedes>
-        <Nav.Link href="https://www.facebook.com/profile.php?id=100093569524691&mibextid=ZbWKwL" target="_blank">
-          <FaFacebook color='#fff' />
-        </Nav.Link>
-        <Nav.Link href="https://www.instagram.com/dds_desenvolvimento/" target="_blank">
-          <FaInstagram color='#fff'/>
-        </Nav.Link>
-      </NavRedes>
-    </Navbar.Collapse>
-  </CustomNavbar>
+    <StyledNavbar bg="" expand="lg" className="shadow-sm">
+      <Navbar.Brand href="/">
+        <div
+          style={{
+            marginLeft: "40px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <img src={Logo} alt="" style={{ width: "100px" }} />
+          <TextLogo>DDS - Agencia de Desenvolvimento</TextLogo>
+        </div>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+        <Nav>
+          <Nav.Link as={Link} to="/">
+            <TextContainer>Inicio</TextContainer>
+          </Nav.Link>
+          <Nav.Link as={Link} to="/quem-somos">
+            <TextContainer>Quem Somos</TextContainer>
+          </Nav.Link>
+          <Nav.Link as={Link} to="/contato">
+            <TextContainer>Contato</TextContainer>
+          </Nav.Link>
+          <StyledNavDropdown title="Tipos de Serviços" id="basic-nav-dropdown">
+            <NavDropdown.Item as={Link} alt="Criação de Sites" to="/criacao-de-sites">
+              Criação de Sites
+            </NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/marketing-digital">
+              Marketing Digital
+            </NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/social-midia">
+              Social Mídia
+            </NavDropdown.Item>
+          </StyledNavDropdown>
+        </Nav>
+        <Nav className="ml-auto" style={{ paddingRight: "10px" }}>
+          <Nav.Link href="https://www.facebook.com/profile.php?id=100093569524691&mibextid=ZbWKwL" target="_blank">
+            <FaFacebook color="#fff" />
+          </Nav.Link>
+          <Nav.Link href="https://www.instagram.com/dds_desenvolvimento/" target="_blank">
+            <FaInstagram color="#fff" />
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </StyledNavbar>
   );
-}
+};
 
 export default NavBar;
--- src/components/Navbar/style.jsx
+++ src/components/Navbar/style.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Nav, Navbar, NavDropdown} from "react-bootstrap";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 export const LogoImage = styled.img`
   height: 30px;
@@ -35,23 +35,46 @@ export const CustomNavbar = styled(Navbar)`
     #650093 75%,
     #43008d 100%
   );
-`
-;
+`;
 
 export const TextLogo = styled.span`
   color: #fff;
   margin-left: 5px;
   margin-top: 10px;
-
-`
-;
+  @media screen and (max-width: 767px) {
+    font-size: 10px;
+  }
+`;
 export const StyledNavText = styled.span`
   color: #fff;
 `;
 
-
 export const StyledNavDropdown = styled(NavDropdown)`
-.dropdown-toggle {
-  color: #fff; /* Replace with your desired color */
-}
+  .dropdown-toggle {
+    font-size: 20px;
+    color: #fff;
+    text-shadow: -1px -1px 0px #a924f0, -1px 1px 0px #a924f0,
+      1px -1px 0px #a924f0, 1px 0px 0px #a924f0;
+  }
+`;
+
+export const StyledNavbar = styled(Navbar)`
+  background-image: linear-gradient(
+    65deg,
+    #bd41ae 0,
+    #a42fa7 25%,
+    #86169d 50%,
+    #650093 75%,
+    #43008d 100%
+  );
+`;
+export const TextContainer = styled.span`
+  font-size: 20px;
+  color: #fff;
+  text-shadow: -1px -1px 0px #a924f0, -1px 1px 0px #a924f0, 1px -1px 0px #a924f0,
+    1px 0px 0px #a924f0;
+`;
+
+export const IconWidth = styled.span`
+  font-size: 25px;
 `;
