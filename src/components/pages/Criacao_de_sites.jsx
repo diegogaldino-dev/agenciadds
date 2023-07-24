1bd2df32b258bf0dc34addd82ef55ef20c76f843
Diego Galdino <diegogaldino-dev@gmail.com>
2023-07-24 11:26:17 -0300

    putting animation on the website creation page

--- src/components/pages/Criacao_de_sites.jsx
+++ src/components/pages/Criacao_de_sites.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import { Image, TitleCreate, Text, TextBGColor, TextImportancia, TextBackground, ParagrafoDoTextBackground, DivTextCreater } from "./style";
 import { Button } from "react-bootstrap";
 import { FaWhatsapp, FaChevronRight } from "react-icons/fa";
@@ -6,9 +9,16 @@ import { AiFillFileText } from 'react-icons/ai';
 import { BsCodeSlash } from 'react-icons/bs';
 import { FiCheckCircle } from 'react-icons/fi';
 
-import criacaoImg from "../../img/cropped-view-hands-typing-laptop.jpg"
+import criacaoImg from "../../img/cropped-view-hands-typing-laptop.jpg";
 
 const Criacao_de_sites = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    });
+  }, []);
+
   const sendWhatsAppMessage = () => {
     const mensagem = encodeURIComponent(
       "Olá, gostaria de solicitar um orçamento."
@@ -27,7 +37,7 @@ const Criacao_de_sites = () => {
           alt="Imagem de Criação de Sites"
         />
         <div className="position-absolute top-0 start-0">
-          <container>
+          <container data-aos="fade-up">
             <div
               className="d-flex flex-column align-items-start align-items-md-center ml-2 ml-md-4"
             >
@@ -41,8 +51,15 @@ const Criacao_de_sites = () => {
                 fácil atualização, permitindo que você mantenha seu conteúdo
                 sempre atualizado.
               </Text>
-              <Button onClick={sendWhatsAppMessage} className="mt-3" style={{ background: 'rgb(135, 23, 157)', border: 'none' }}>
-                <FaWhatsapp size={20} style={{ marginRight: "0.5rem", marginBottom: "3px" }} />
+              <Button
+                onClick={sendWhatsAppMessage}
+                className="mt-3"
+                style={{ background: "rgb(135, 23, 157)", border: "none" }}
+              >
+                <FaWhatsapp
+                  size={20}
+                  style={{ marginRight: "0.5rem", marginBottom: "3px" }}
+                />
                 Solicitar Orçamento
               </Button>
             </div>
@@ -50,7 +67,7 @@ const Criacao_de_sites = () => {
         </div>
       </div>
 
-      <TextImportancia>
+      <TextImportancia data-aos="fade-up">
         <div className="container pb-5 pt-5 text-center">
           <h3 style={{ color: "#87179D" }}>Vantagens da criação de sites profissionais</h3>
           <p>
@@ -59,7 +76,7 @@ const Criacao_de_sites = () => {
         </div>
       </TextImportancia>
 
-      <TextBGColor className="img-fluid">
+      <TextBGColor className="img-fluid" data-aos="fade-up">
         <div className="container pb-5">
           <div className="row">
             <div className="col-md-6" style={{ marginTop: "220px" }}>
@@ -71,22 +88,22 @@ const Criacao_de_sites = () => {
               </ParagrafoDoTextBackground>
             </div>
             <DivTextCreater className="col-md-6 shadow-lg">
-              <h4>Briefing <FaChevronRight color="#702AAF" /></h4>
+              <h4 data-aos="fade-up">Briefing <FaChevronRight color="#702AAF" /></h4>
               <p>Entender a expectativa</p>
               <p>
                 Através das perguntas existentes no Briefing, a nossa equipe poderá traduzir as suas expectativas e criar um site profissional e personalizado.
               </p>
-              <h4>Conteúdos <AiFillFileText color="#166AF2" /></h4>
+              <h4 data-aos="fade-up">Conteúdos <AiFillFileText color="#166AF2" /></h4>
               <p>Textos e imagens</p>
               <p>
                 Nosso time de redação vai ajudar na criação de textos para o seu site de forma bem próxima. A criação de sites depende de textos bem elaborados.
               </p>
-              <h4>Criação do site <BsCodeSlash color="#87179D" /></h4>
+              <h4 data-aos="fade-up">Criação do site <BsCodeSlash color="#87179D" /></h4>
               <p>Equipe de criação</p>
               <p>
                 A equipe trabalhará na criação do site com base nas informações e conteúdos. Você receberá um link com a prévia do seu projeto.
               </p>
-              <h4>Finalização <FiCheckCircle color="green" /></h4>
+              <h4 data-aos="fade-up">Finalização <FiCheckCircle color="green" /></h4>
               <p>Publicação e conclusão</p>
               <p>
                 Nesta etapa iremos concluir as páginas internas do seu site, formulários, módulos e etc. A equipe irá testar o site e deixá-lo pronto para ser lançado.
