49f4c306ee964e54049790ea0a74b2d6e7286d0a
Diego Galdino <diegogaldino-dev@gmail.com>
2023-08-14 15:51:32 -0300

    adjusting carousel image size and adjusting texts and styles

--- src/components/pages/Carrousel/CarouselHome.jsx
+++ src/components/pages/Carrousel/CarouselHome.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Carousel } from "react-bootstrap";
 import { ImagemCarrousel, CarouselCaption, CarouselTitle, CarouselText } from "./style";
 
-import Imagem1 from "../../../img/img01.jpg";
-import Imagem2 from "../../../img/img02.jpg";
-import Imagem3 from "../../../img/img03.jpg";
+import Imagem1 from "../../../img/slide/c2.jpg";
+import Imagem2 from "../../../img/slide/s.jpg";
+import Imagem3 from "../../../img/slide/m.jpg";
 
 const CarouselHome = () => {
   return (
@@ -14,10 +14,11 @@ const CarouselHome = () => {
           className="d-block w-100"
           src={Imagem1}
           alt="Slide 1"
+          fluid
         />
         <CarouselCaption className="carousel-caption">
-          <CarouselTitle>Slide 1</CarouselTitle>
-          <CarouselText>Text for slide 1</CarouselText>
+          <CarouselTitle>Transforme sua Presença Online</CarouselTitle>
+          <CarouselText>Sites Impactantes, Mídias Sociais Estratégicas e Marketing Digital de Resultados</CarouselText>
         </CarouselCaption>
       </Carousel.Item>
       <Carousel.Item>
@@ -25,10 +26,11 @@ const CarouselHome = () => {
           className="d-block w-100"
           src={Imagem2}
           alt="Slide 2"
+          fluid
         />
         <CarouselCaption className="carousel-caption">
-          <CarouselTitle>Slide 2</CarouselTitle>
-          <CarouselText>Text for slide 2</CarouselText>
+          <CarouselTitle>Conquiste a Atenção das Redes Sociais</CarouselTitle>
+          <CarouselText>Campanhas Criativas que Geram Engajamento e Crescimento</CarouselText>
         </CarouselCaption>
       </Carousel.Item>
       <Carousel.Item>
@@ -36,10 +38,11 @@ const CarouselHome = () => {
           className="d-block w-100"
           src={Imagem3}
           alt="Slide 3"
+          fluid
         />
         <CarouselCaption className="carousel-caption">
-          <CarouselTitle>Slide 3</CarouselTitle>
-          <CarouselText>Text for slide 3</CarouselText>
+          <CarouselTitle>Alcance Seu Público Alvo</CarouselTitle>
+          <CarouselText>Estratégias Digitais Personalizadas para Impulsionar Seus Negócios</CarouselText>
         </CarouselCaption>
       </Carousel.Item>
     </Carousel>
--- src/components/pages/Carrousel/style.jsx
+++ src/components/pages/Carrousel/style.jsx
@@ -1,21 +1,51 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const ImagemCarrousel = styled.img`
-  max-height: 100vh;
+  width: 100%;
+  height: 800px;
   object-fit: cover;
-`
+
+  @media (max-width: 768px) {
+    height: 500px;
+  }
+
+  @media (max-width: 576px) {
+    height: 300px;
+  }
+`;
 
 export const CarouselCaption = styled.div`
   &.carousel-caption {
     text-align: left;
-    /* Adicione aqui os estilos específicos do Bootstrap para a classe .carousel-caption */
+    padding: 20px;
+    color: #000000;
+    text-shadow: 1px 1px 2px rgba(255, 255, 255, 0.5);
+    background: linear-gradient(rgba(182, 31, 252, 0.5), rgba(206, 125, 243, 0.5)); /* Gradiente de fundo para destacar o texto */
+
+    @media (max-width: 576px) {
+      padding: 10px; /* Reduzindo o padding para telas menores */
+    }
   }
 `;
 
 export const CarouselTitle = styled.h3`
-  /* Estilos do título */
+  font-size: 24px;
+  margin-bottom: 10px;
+  text-shadow: 1px 1px 2px rgba(255, 255, 255, 0.5);
+  color: #000000;
+
+  @media (max-width: 576px) {
+    font-size: 20px;
+  }
 `;
 
 export const CarouselText = styled.p`
-  /* Estilos do texto */
+  font-size: 16px;
+  line-height: 1.5;
+  text-shadow: 1px 1px 2px rgba(255, 255, 255, 0.5);
+  color: #000000;
+
+  @media (max-width: 576px) {
+    font-size: 14px;
+  }
 `;
