c1ecd55f750de5cb900019c017625fa41b83a908
Diego Galdino <diegogaldino-dev@gmail.com>
2024-01-29 14:04:54 -0300

    adjusting the whats button

--- src/components/pages/WhatsAppButton.jsx
+++ src/components/pages/WhatsAppButton.jsx
@@ -5,14 +5,15 @@ import styled from "styled-components";
 const WhatsAppButtonContainer = styled.div`
   position: fixed;
   bottom: 20px;
-  right: 20px;
-  z-index: 999;
+  right: 10px;
+  z-index: 1000;
   cursor: pointer;
   margin-bottom: 20px;
   margin-right: 20px;
 
   @media (max-width: 576px) {
-    bottom: 10px;
+    position: fixed;
+    bottom: 80px;
     right: 10px;
     margin-bottom: 10px;
     margin-right: 10px;
@@ -31,17 +32,9 @@ const WhatsAppIcon = styled(FaWhatsapp)`
 
 const WhatsAppButton = () => {
   const handleWhatsAppClick = () => {
-    // Número de telefone para o qual você deseja enviar a mensagem
     const phoneNumber = "5511978788732";
-    // Mensagem que você deseja enviar
     const message = "Olá, gostaria de mais informações.";
-
-    // Gerar a URL do WhatsApp com o número de telefone e a mensagem
-    const whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(
-      message
-    )}`;
-
-    // Abrir o WhatsApp em uma nova janela ou guia
+    const whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
     window.open(whatsappUrl, "_blank");
   };
 
