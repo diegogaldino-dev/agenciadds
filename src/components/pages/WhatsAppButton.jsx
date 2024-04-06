5d22b487b1a3ff5679aa1bc6ee72e17ba9aa4099
Diego Galdino <diegogaldino-dev@gmail.com>
2024-04-05 23:14:00 -0300

    adding whats button

--- src/components/pages/WhatsAppButton.jsx
+++ src/components/pages/WhatsAppButton.jsx
@@ -1,47 +1,25 @@
 import React from "react";
-import { FaWhatsapp } from "react-icons/fa";
-import styled from "styled-components";
-
-const WhatsAppButtonContainer = styled.div`
-  position: fixed;
-  bottom: 20px;
-  right: 10px;
-  z-index: 1000;
-  cursor: pointer;
-  margin-bottom: 20px;
-  margin-right: 20px;
-
-  @media (max-width: 576px) {
-    position: fixed;
-    bottom: 80px;
-    right: 10px;
-    margin-bottom: 10px;
-    margin-right: 10px;
-  }
-`;
-
-const WhatsAppIcon = styled(FaWhatsapp)`
-  font-size: 70px;
-  color: green;
-
-  @media (max-width: 576px) {
-    font-size: 50px;
-    margin-right: 15px;
-  }
-`;
+import { FloatingWhatsApp } from "react-floating-whatsapp";
+import AvatarButton from "../../img/logo_oficial 1.svg"
 
 const WhatsAppButton = () => {
-  const handleWhatsAppClick = () => {
-    const phoneNumber = "5511978788732";
-    const message = "Olá, gostaria de mais informações.";
-    const whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
-    window.open(whatsappUrl, "_blank");
-  };
-
   return (
-    <WhatsAppButtonContainer onClick={handleWhatsAppClick}>
-      <WhatsAppIcon />
-    </WhatsAppButtonContainer>
+    <FloatingWhatsApp
+      phoneNumber="5511978788732"
+      accountName="DDS Desenvolvimento"
+      avatar={AvatarButton}
+      message="Olá, gostaria de mais informações."
+      darkMode={false}
+      chatMessage="Olá, gostaria de mais informações."
+      allowClickAway={true}
+      placeholder="Digite sua mensagem aqui..."
+      statusMessage="Online" // Mensagem de status
+      styles={{
+        backgroundColor: "#25d366",
+        color: "#fff",
+        iconColor: "#fff",
+      }}
+    />
   );
 };
 
